8abd8cb5a3dd1a4904a2b896e1d5d5dc4edfd53b
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-04-21 23:43:25 -0600

    Mostrar infomacion personalizada cuando buscas el valor de las criptos

--- src/main/java/com/api/cripto/services/GenerateJSON.java
+++ src/main/java/com/api/cripto/services/GenerateJSON.java
@@ -42,4 +42,33 @@ public class GenerateJSON {
 
         return result;
     }
+
+    public JSONObject accommodateJSONValorCriptoResults(JSONObject coin) {
+        JSONObject filteredCoin = new JSONObject();
+
+        filteredCoin.put("eth_dominance", coin.get("eth_dominance"));
+        filteredCoin.put("active_cryptocurrencies", coin.get("active_cryptocurrencies"));
+        filteredCoin.put("stablecoin_market_cap", coin.get("stablecoin_market_cap"));
+        filteredCoin.put("derivatives_volume_24h", coin.get("derivatives_volume_24h"));
+        filteredCoin.put("eth_dominance_yesterday", coin.get("eth_dominance_yesterday"));
+        filteredCoin.put("last_updated", coin.get("last_updated"));
+        filteredCoin.put("defi_volume_24h", coin.get("defi_volume_24h"));
+        filteredCoin.put("active_exchanges", coin.get("active_exchanges"));
+        filteredCoin.put("derivatives_24h_percentage_change", coin.get("derivatives_24h_percentage_change"));
+        filteredCoin.put("defi_market_cap", coin.get("defi_market_cap"));
+        filteredCoin.put("total_cryptocurrencies", coin.get("total_cryptocurrencies"));
+        filteredCoin.put("btc_dominance_24h_percentage_change", coin.get("btc_dominance_24h_percentage_change"));
+        filteredCoin.put("btc_dominance_yesterday", coin.get("btc_dominance_yesterday"));
+        filteredCoin.put("total_exchanges", coin.get("total_exchanges"));
+        filteredCoin.put("stablecoin_volume_24h", coin.get("stablecoin_volume_24h"));
+        filteredCoin.put("stablecoin_volume_24h_reported", coin.get("stablecoin_volume_24h_reported"));
+        filteredCoin.put("defi_volume_24h_reported", coin.get("defi_volume_24h_reported"));
+        filteredCoin.put("active_market_pairs", coin.get("active_market_pairs"));
+        filteredCoin.put("btc_dominance", coin.get("btc_dominance"));
+        filteredCoin.put("stablecoin_24h_percentage_change", coin.get("stablecoin_24h_percentage_change"));
+        filteredCoin.put("defi_24h_percentage_change", coin.get("defi_24h_percentage_change"));
+        filteredCoin.put("eth_dominance_24h_percentage_change", coin.get("eth_dominance_24h_percentage_change"));
+
+        return filteredCoin;
+    }
 }
--- src/main/java/com/api/cripto/services/ServiceCoinmarketCap.java
+++ src/main/java/com/api/cripto/services/ServiceCoinmarketCap.java
@@ -46,8 +46,11 @@ public class ServiceCoinmarketCap {
 
     public JSONObject getValorCriptoResults(String convert) throws MalformedURLException {
         URL url = new URL(urlPeticion + "global-metrics/quotes/latest?convert=" + convert);
+        JSONObject response = getPeticion(url);
 
-        return getPeticion(url);
+        JSONObject general = (JSONObject) response.get("data");
+
+        return generateJSON.accommodateJSONValorCriptoResults(general);
     }
 
     public JSONObject getPeticion(URL url) {
