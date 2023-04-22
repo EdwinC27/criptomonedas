f5e8706d89d029f28054b09a9b34b466fbbec057
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-04-21 20:09:59 -0600

    Cambiar el tipo de dato String de la urlPeticion por un tipo de dato URL

--- src/main/java/com/api/cripto/services/ServiceCoinmarketCap.java
+++ src/main/java/com/api/cripto/services/ServiceCoinmarketCap.java
@@ -11,37 +11,40 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @Service
 public class ServiceCoinmarketCap {
     @Value("${ApiKeyServiceCoinmarketCap}")
     private String ApiKey;
     @Value("${UrlServiceCoinmarketCap}")
-    private String URL;
+    private String urlPeticion;
 
-    public JSONObject getAllResults() {
-        String url = URL + "cryptocurrency/listings/latest?start=1&limit=5&convert=USD";
-
-        return getPeticion(url);
-    }
-
-    public JSONObject getIdCriptoResults(String id) {
-        String url = URL + "cryptocurrency/info?id=" + id;
+    public JSONObject getAllResults() throws MalformedURLException {
+        URL url = new URL( urlPeticion + "cryptocurrency/listings/latest?start=1&limit=5&convert=USD");
 
         return getPeticion(url);
     }
 
-    public JSONObject getValorCriptoResults(String convert) {
-        String url = URL + "global-metrics/quotes/latest?convert=" + convert;
+    public JSONObject getIdCriptoResults(String id) throws MalformedURLException {
+        URL url = new URL(urlPeticion + "cryptocurrency/info?id=" + id);
 
         return getPeticion(url);
     }
 
-    public JSONObject getPeticion(String url) {
+    public JSONObject getValorCriptoResults(String convert) throws MalformedURLException {
+        URL url = new URL(urlPeticion + "global-metrics/quotes/latest?convert=" + convert);
+
+        return getPeticion(url);
+    }
+
+    public JSONObject getPeticion(URL url) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-CMC_PRO_API_KEY", ApiKey);
         HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(url.toString(), HttpMethod.GET, entity, String.class);
         JSONObject jsonObject = (JSONObject) JSONValue.parse(response.getBody());
         return jsonObject;
     }
