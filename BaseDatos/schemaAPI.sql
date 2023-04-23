88878b109f98aa3f6086ff4a1ed4f0ecc97261ea
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-04-23 14:22:53 -0600

    Agregar el archivo necesario para la creacion de la Base de Datos

---
+++ BaseDatos/schemaAPI.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS public.informacion
+(
+    endpoint character varying COLLATE pg_catalog."default",
+    hora character varying COLLATE pg_catalog."default",
+    status character varying COLLATE pg_catalog."default"
+)
