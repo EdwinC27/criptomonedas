bc82a7f53de13996081c14947fa0ac00b76c21c9
Edwin Alejandro <Edwin_Alejandro@gmail.com>
2023-04-25 12:48:41 -0600

    Agregar los comandos de sql necesarios para implementar el token con docker

--- dockerDir/create_table.sql
+++ dockerDir/create_table.sql
@@ -3,4 +3,14 @@ CREATE TABLE informacion
     endpoint character varying,
     hora character varying,
     status character varying
-)
+);
+
+CREATE TABLE users (
+    users character varying,
+    password character varying,
+    token character varying
+);
+
+INSERT INTO public.users(
+	users, password, token)
+	VALUES ('edwin', '20254820b2c8ea8a3ff8705e46509a2bf4fd9a6b8243e64b7dabe118f8ff7639', '');
